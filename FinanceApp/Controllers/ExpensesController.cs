c7a4e87103951f30258fbc0b25d45da7fa00a2d3
mlmoore2259 <mlmoore2259@gmail.com>
2026-06-08 17:06:36 -0400

    Created expenses index method

---
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -0,0 +1,21 @@
+﻿using FinanceApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly FinanceAppContext _context;
+
+        public ExpensesController(FinanceAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var expenses = _context.Expenses.ToList();
+            return View(expenses);
+        } 
+    }
+}
